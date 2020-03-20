876a9554e818230fb56b57a80e9bc18eb1a88d52
Maxim Filimonov <tpaktopsp@gmail.com>
2020-03-21 07:57:59 +0800

    exploring elm like code in typescript - removing mutation and generalising calculation

--- src/checkout.test.ts
+++ src/checkout.test.ts
@@ -1,27 +1,40 @@
 import Checkout from "./checkout";
 describe("Checkout", () => {
-  it("totals to zero by default", () => {
-    const co = new Checkout({ products: [{ name: "test", price: 1000 }] });
+  describe("adding products", () => {
+    it("ignores invalid name", () => {
+      let checkout = new Checkout({
+        products: [{ name: "test", price: 1000 }]
+      }).append("incorrect name");
 
-    expect(co.total()).toEqual(0);
+      checkout.total();
+    });
   });
-  it("calculates total based on ads prices", () => {
-    const mockAdConfig = {
-      products: [
-        {
-          name: "classic",
-          price: 49.99
-        },
-        {
-          name: "standout",
-          price: 50.01
-        }
-      ]
-    };
-    const co = new Checkout(mockAdConfig);
-    mockAdConfig.products.map(x => x.name).forEach(co.add);
+  describe("total calculation", () => {
+    it("totals to zero by default", () => {
+      const checkout = new Checkout({
+        products: [{ name: "test", price: 1000 }]
+      });
 
-    const total = co.total();
-    expect(total).toEqual(100);
+      expect(checkout.total()).toEqual(0);
+    });
+    it("calculates total based on ads prices", () => {
+      const mockAdConfig = {
+        products: [
+          {
+            name: "classic",
+            price: 49.99
+          },
+          {
+            name: "standout",
+            price: 50.01
+          }
+        ]
+      };
+      let checkout = new Checkout(mockAdConfig);
+      checkout = checkout.append("classic").append("standout");
+
+      const total = checkout.total();
+      expect(total).toEqual(100);
+    });
   });
 });
--- src/checkout.ts
+++ src/checkout.ts
@@ -3,21 +3,35 @@ import { AdConfig, Product } from "./ad_config";
 class Checkout {
   adConfig: AdConfig;
   products: Product[];
-  constructor(adConfig: AdConfig) {
+  constructor(adConfig: AdConfig, products: Product[] = []) {
     this.adConfig = adConfig;
-    this.products = [];
+    this.products = products;
   }
-  add = (productName: string) => {
+  append = (productName: string) => {
     const matchedProduct = this.adConfig.products.find(
       x => x.name === productName
     );
-    this.products.push(matchedProduct);
+    if (matchedProduct) {
+      const newProducts = this.products.concat([matchedProduct]);
+      return new Checkout(this.adConfig, newProducts);
+    }
+    return this;
   };
   total = (): number => {
-    return this.products.reduce((acc, product) => {
-      acc += product.price;
-      return acc;
-    }, 0);
+    return calculateTotal(this.products);
   };
+}
+function sum(numbers: number[]) {
+  return numbers.reduce((acc, product) => {
+    acc += product;
+    return acc;
+  }, 0);
+}
+function getPrice(product: Product) {
+  return product.price;
+}
+
+export function calculateTotal(products: Product[]) {
+  return sum(products.map(getPrice));
 }
 export default Checkout;
