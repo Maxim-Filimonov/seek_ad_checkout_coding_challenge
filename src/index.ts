8e0de7c6117df0c92a2e12335c9b3b3290c98851
Maxim Filimonov <tpaktopsp@gmail.com>
2020-03-21 09:34:39 +0800

    add sample scenarios and output them in index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("TS works does work");
+import sample_scenarios from "./data/sample_scenarios.json";
+import loadConfig from "./ad_config";
+import Checkout from "./checkout";
+
+console.log("Running scenarios ");
+const config = loadConfig();
+sample_scenarios.forEach(scenario => {
+  let checkout = new Checkout(config);
+  const total = scenario.items
+    .reduce((acc, item) => acc.append(item), checkout)
+    .total(scenario.customer);
+
+  console.log("==========");
+  console.log("Sample scenario:", scenario);
+  console.log("Total calculated:", total);
+  console.log("==========");
+});
